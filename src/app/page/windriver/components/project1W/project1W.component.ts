57d3ca1a585986dffaa3dd5c11dc59fe7d54e8b4
toledoantonio <antonioramont7@gmail.com>
2023-05-29 13:18:12 -0300

    first structure card ready

--- src/app/page/windriver/components/project1W/project1W.component.ts
+++ src/app/page/windriver/components/project1W/project1W.component.ts
@@ -8,6 +8,39 @@ import { Component, OnInit } from '@angular/core';
 export class Project1WComponent implements OnInit {
   title = "project1W";
 
+  sections = [
+    {
+      title: 'Título 1',
+      image: 'ruta-de-la-imagen-1',
+      description: 'Descripción 1'
+    },
+    {
+      title: 'Título 2',
+      image: 'ruta-de-la-imagen-2',
+      description: 'Descripción 2'
+    },
+    {
+      title: 'Título 3',
+      image: 'ruta-de-la-imagen-3',
+      description: 'Descripción 3'
+    },
+    {
+      title: 'Título 4',
+      image: 'ruta-de-la-imagen-4',
+      description: 'Descripción 4'
+    },
+    {
+      title: 'Título 5',
+      image: 'ruta-de-la-imagen-5',
+      description: 'Descripción 5'
+    },
+    {
+      title: 'Título 6',
+      image: 'ruta-de-la-imagen-6',
+      description: 'Descripción 6'
+    }
+  ];
+
   constructor() { }
 
   ngOnInit(): void {

07518d489746923e7ea8fb41628fde4e5cdd4865
toledoantonio <antonioramont7@gmail.com>
2023-05-30 01:59:47 -0300

    new scss cards

--- src/app/page/naranjax/components/project1N/project1N.component.ts
+++ src/app/page/naranjax/components/project1N/project1N.component.ts
@@ -6,7 +6,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./project1N.component.scss']
 })
 export class Project1NComponent implements OnInit {
-  title = 'project1N';
+  title = "NaranjaX - Project 1";
+
+  sections = [
+    {
+      title: 'Título 1',
+      image: 'https://lh3.googleusercontent.com/24_0j8bPtaB-RhS_AZG9WvGT3e9L_Co_5wOl6OLAPnl2Z93C2KDc2LIOOqg7J8QgRcObGJIZng=s0',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla euismod, nisl eget aliquam ultricies, nunc nisl aliquet nunc, quis aliquam nis'
+    },
+    {
+      title: 'Título 2',
+      image: 'https://images.ctfassets.net/23aumh6u8s0i/2rne3k2iZ3EtRRpLuafLhM/67acd166eb2dcacba5d573863f1b0b06/angular-and-auth0',
+      description: 'Descripción 2'
+    },
+    {
+      title: 'Título 3',
+      image: 'https://raw.githubusercontent.com/ciglesiasweb/angular-integration-tests-sample/main/docs/images/elements-to-test.png',
+      description: 'lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla euismod, nisl eget aliquam ultricies, nunc nisl aliquet nunc, quis aliquam nis'
+    },
+    {
+      title: 'Título 4',
+      image: 'ruta-de-la-imagen-4',
+      description: 'Descripción 4'
+    },
+    {
+      title: 'Título 5',
+      image: 'ruta-de-la-imagen-5',
+      description: 'Descripción 5'
+    },
+    {
+      title: 'Título 6',
+      image: 'ruta-de-la-imagen-6',
+      description: 'Descripción 6'
+    }
+  ];
 
   constructor() { }
 
--- src/app/page/windriver/components/project1W/project1W.component.ts
+++ src/app/page/windriver/components/project1W/project1W.component.ts
@@ -6,23 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./project1W.component.scss']
 })
 export class Project1WComponent implements OnInit {
-  title = "project1W";
+  title = "Windriver - Project 1";
 
   sections = [
     {
       title: 'Título 1',
-      image: 'ruta-de-la-imagen-1',
-      description: 'Descripción 1'
+      image: 'https://lh3.googleusercontent.com/24_0j8bPtaB-RhS_AZG9WvGT3e9L_Co_5wOl6OLAPnl2Z93C2KDc2LIOOqg7J8QgRcObGJIZng=s0',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla euismod, nisl eget aliquam ultricies, nunc nisl aliquet nunc, quis aliquam nis'
     },
     {
       title: 'Título 2',
-      image: 'ruta-de-la-imagen-2',
+      image: 'https://images.ctfassets.net/23aumh6u8s0i/2rne3k2iZ3EtRRpLuafLhM/67acd166eb2dcacba5d573863f1b0b06/angular-and-auth0',
       description: 'Descripción 2'
     },
     {
       title: 'Título 3',
-      image: 'ruta-de-la-imagen-3',
-      description: 'Descripción 3'
+      image: 'https://raw.githubusercontent.com/ciglesiasweb/angular-integration-tests-sample/main/docs/images/elements-to-test.png',
+      description: 'lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla euismod, nisl eget aliquam ultricies, nunc nisl aliquet nunc, quis aliquam nis'
     },
     {
       title: 'Título 4',
